8d9fdc8dcd6bc080e13952cd29121d2a519359e7
Nuno Leitao <nunogrl@gmail.com>
2024-02-20 23:27:40 +0000

    using new theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -37,4 +37,4 @@ PLUGINS = ['related_posts', 'series', 'sitemap', 'tag_cloud', 'assets', 'share_p
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = "themes/eventually"
+THEME = "themes/neat"
